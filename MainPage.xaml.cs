6d5dabccfe2374a32d5c1e12700b45e5c2044d22
Alisher <Alisher@WIN-TQULFPK2KRJ>
2023-11-13 16:17:33 +0500

    MalComputer test

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -12,12 +12,7 @@
 
 		private void OnCounterClicked(object sender, EventArgs e)
 		{
-			count++;
-
-			if (count == 1)
-				CounterBtn.Text = $"Clicked {count} time";
-			else
-				CounterBtn.Text = $"Clicked {count} times";
+			ProgressBarCorrect.WidthRequest += 50;
 
 			SemanticScreenReader.Announce(CounterBtn.Text);
 		}
